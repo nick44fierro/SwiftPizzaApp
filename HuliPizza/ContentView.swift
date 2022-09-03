751648c33cd4bcb6ddb5e34c3ebab3921f0fba04
Nicholas Fierro <nick44fierro@gmail.com>
2022-09-03 13:33:15 -0400

    Initial Commit

---
+++ HuliPizza/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HuliPizza
+//
+//  Created by Nick Fierro on 9/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HuliPizza/HuliPizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HuliPizzaApp.swift
+//  HuliPizza
+//
+//  Created by Nick Fierro on 9/3/22.
+//
+
+import SwiftUI
+
+@main
+struct HuliPizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
